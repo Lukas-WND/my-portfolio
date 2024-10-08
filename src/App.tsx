309f3990161269aaca070940d9dce43b91268357
Lukas Wendel <lukas.palheta@flex-am.com.br>
2024-10-08 18:54:07 -0400

    :construction: home content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-full h-screen bg-dark-ciano">
+      <div className="w-full h-full flex flex-col items-center justify-center text-[300px] leading-[0.85] tracking-wider antialiased hover:subpixel-antialiased">
+        <h2 className="text-almond">I'm Lukas</h2>
+        <h2 className="text-transparent stroke-khaki">I'm Lukas</h2>
+        <h2 className="text-almond">I'm Lukas</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-ciano': '#081C1C',
+        'almond': '#EAE0D5',
+        'khaki': '#C6AC8F',
+      },
+      textStroke: {
+        khaki: '3px khaki'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.stroke-khaki': {
+          '-webkit-text-stroke': '3px #C6AC8F',
+          'text-stroke': '3px #C6AC8F',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
